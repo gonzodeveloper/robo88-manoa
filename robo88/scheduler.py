f75db8f60800bb34cb7561968c65b9c5872314f1
wintergreen <doctor.kyle.hart@gmail.com>
2025-01-06 21:22:13 -1000

    work

--- robo88/scheduler.py
+++ robo88/scheduler.py
@@ -70,7 +70,7 @@ class SchedulerRequests(DispatchBot):
         if status == "ERROR":
             self.log({"runtime_error": msg}, level="ERROR")
             raise RuntimeError(msg)
-        print(json)
+        print(msg)
         # Read dataframe from JSON
         return pd.read_json(msg)
 
