da106f60a29202dbeba5552674c538ca933253ef
wintergreen <doctor.kyle.hart@gmail.com>
2025-01-06 22:24:57 -1000

    work

--- robo88/scheduler.py
+++ robo88/scheduler.py
@@ -74,7 +74,7 @@ class SchedulerRequests(DispatchBot):
         print(msg_dict)
 
         # Read dataframe from JSON
-        return pd.DataFrame(msg_dict)
+        return pd.DataFrame.from_dict(msg_dict, orient="records")
 
     def remove_request(self, user, obj_name=None, request_idx=None):
         """
