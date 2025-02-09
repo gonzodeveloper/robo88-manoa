c5b996c301655eea289d1d00b683310aa959d0df
wintergreen <doctor.kyle.hart@gmail.com>
2025-02-08 21:07:27 -1000

    work

--- robo88/scheduler.py
+++ robo88/scheduler.py
@@ -1,74 +1,102 @@
-from .sys_mods import DispatchBot
 import pandas as pd
+import logging
+import socket
+import struct
+import yaml
+import time
 import json
+import sys
+import os
 
 
-class SchedulerRequests(DispatchBot):
+class SchedulerRequests:
 
-    def __init__(self, *args, **kwargs):
-        # Load the Robot params
-        super().__init__("scheduler_requests", *args, **kwargs)
+    def __init__(self, config_file, debug=False):
+        # Read config
+        with open(config_file) as stream:
+            self.config = yaml.safe_load(stream)
 
-    def get_recent_observation(self, user, n_nights=None, obj_name=None):
-        """
-        Retrieve observations for a given user within search spec
-        :param user: name of the user who submitted obs request; str.
-        :param n_nights: how many nights back to search, if set; str.
-        :param obj_name: name of object to search, if set; str.
-        :return: list of files
-        """
-        # Send request to change night (don't send nulls in JSON)
-        args = locals()
-        request = {k: v for k, v in args.items() if v is not None and k != "self"}
-        status, msg = self.send_request(f"get_obs {json.dumps(request)}")
+        # Connect to controller
+        host = self.config["controller_ip"]
+        port = self.config["controller_port"]
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
 
-        if status == "ERROR":
-            self.log({"runtime_error": msg}, level="ERROR")
-            raise RuntimeError(msg)
+        # Times out after 12 hours
+        self.sock.settimeout(12 * 6000)
 
-        # Return list of files (sub out /obs/data/ for local data_dir)
-        msg.replace("/obs/data/", self.config["data_dir"])
-        return msg.split()
+        # Get logger
+        self.logger = logging.getLogger("robo_scheduler")
+        # Log formatting
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.logger.setLevel(logging.DEBUG)
+        # Log to file
+        logfile = os.path.join(self.config['log_dir'], "robo_scheduler.log")
+        handler = logging.FileHandler(logfile)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
 
-    def add_request(self, obj_name, ra_hms, dec_dms, user, priority,
-                    camera_mode=None, filter_name=None, exp_time=None, exp_num=None, comments=None):
+        # Print to standard out if we are in debug mode
+        if debug:
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+
+    def add_request(self, request):
         """
         Load single observation into request database
-        :param obj_name: standard object name; str.
-        :param ra_hms: right ascension; HH:MM:SS.S; str.
-        :param dec_dms: declination; DD:MM:SS.S; str.
-        :param user: name of requesting user (no spaces); str.
-        :param priority: ranked priority of observation 1-5 for survey priorities; 9 for immediate; int.
-        :param camera_mode: binning mode for camera, e.g. BIN2 or BIN5
-        :param filter_name: filter to use for target (Open, g, r, i, z, y)
-        :param exp_time: exposure time in seconds; int.
-        :param exp_num: number of dithered exposures to take; int.
-        :param comments: general comments; str
+        :param request: observation request dictionary, field definitions below:
+                +-------------------+--------------+------+-----+---------+----------------+
+                | Field             | Type         | Null | Key | Default | Extra          |
+                +-------------------+--------------+------+-----+---------+----------------+
+                | ra_hms            | varchar(16)  | NO   |     | NULL    |                |
+                | dec_dms           | varchar(16)  | NO   |     | NULL    |                |
+                | obj_name          | varchar(32)  | NO   |     | NULL    |                |
+                | username          | varchar(32)  | NO   |     | NULL    |                |
+                | priority          | int(11)      | NO   |     | NULL    |                |
+                | window_start      | datetime     | NO   |     | NULL    |                |
+                | window_finish     | datetime     | NO   |     | NULL    |                |
+                | binning_mode      | varchar(16)  | NO   |     | NULL    |                |
+                | filters           | varchar(32)  | NO   |     | NULL    |                |
+                | exp_times         | varchar(32)  | NO   |     | NULL    |                |
+                | exp_num           | int(11)      | NO   |     | NULL    |                |
+                | dither            | varchar(32)  | NO   |     | NULL    |                |
+                | stack             | varchar(32)  | NO   |     | NULL    |                |
+                | cadence           | varchar(32)  | NO   |     | NULL    |                |
+                | ns_ref_time       | varchar(32)  | YES  |     | NULL    |                |
+                | ns_rate_ra        | float        | YES  |     | NULL    |                |
+                | ns_rate_dec       | float        | YES  |     | NULL    |                |
+                | max_airmass       | float        | YES  |     | NULL    |                |
+                | min_moon_sep      | float        | YES  |     | NULL    |                |
+                | follow_on_id      | int(11)      | YES  |     | NULL    |                |
+                | follow_on_lag_min | int(11)      | YES  |     | NULL    |                |
+                | follow_on_lag_max | int(11)      | YES  |     | NULL    |                |
+                | too               | varchar(16)  | YES  |     | NULL    |                |
+                | comments          | varchar(256) | YES  |     | NULL    |                |
+                +-------------------+--------------+------+-----+---------+----------------+
         :return: request index of target inserted.
         """
         # Send request to change night (don't send nulls in JSON)
-        args = locals()
-        request = {k: v for k, v in args.items() if v is not None and k != "self"}
-        status, msg = self.send_request(f"add_req {json.dumps(request)}")
+        status, msg = self._send_request(f"add_req {json.dumps(request)}")
 
         if status == "ERROR":
-            self.log({"runtime_error": msg}, level="ERROR")
+            self.logger.error(msg)
             raise RuntimeError(msg)
 
         # Return target idx
         return msg
 
-    def get_requests(self, user):
+    def get_requests(self, username):
         """
         Get requests submitted by the given user.
-        :param user: name of user associated with requests; str.
+        :param username: name of user associated with requests; str.
         :return: pd.Dataframe
         """
         # Send request for obs_requests
-        status, msg = self.send_request(f"get_reqs {user}")
+        status, msg = self._send_request(f"get_reqs {username}")
 
         if status == "ERROR":
-            self.log({"runtime_error": msg}, level="ERROR")
+            self.logger.error(msg)
             raise RuntimeError(msg)
 
         msg_dict = json.loads(json.loads(msg))
@@ -76,10 +104,31 @@ class SchedulerRequests(DispatchBot):
         # Read dataframe from JSON
         return pd.json_normalize(msg_dict)
 
-    def remove_request(self, user, obj_name=None, request_idx=None):
+    def get_recent_observation(self, username, n_nights=None, obj_name=None):
+        """
+        Retrieve observations for a given user within search spec
+        :param username: name of the user who submitted obs request; str.
+        :param n_nights: how many nights back to search, if set; str.
+        :param obj_name: name of object to search, if set; str.
+        :return: list of files
+        """
+        # Send request to change night (don't send nulls in JSON)
+        args = locals()
+        request = {k: v for k, v in args.items() if v is not None and k != "self"}
+        status, msg = self._send_request(f"get_obs {json.dumps(request)}")
+
+        if status == "ERROR":
+            self.logger.error(msg)
+            raise RuntimeError(msg)
+
+        # Return list of files (sub out /obs/data/ for local data_dir)
+        msg.replace("/obs/data/", self.config["data_dir"])
+        return msg.split()
+
+    def remove_request(self, username, obj_name=None, request_idx=None):
         """
         Remove one or multiple observation requests from database.
-        :param user: name of user who submitted obs request; str.
+        :param username: name of user who submitted obs request; str.
         :param obj_name: remove all requests with given object name; str
         :param request_idx: remove single request with given index; int
         :return: number of requests removed
@@ -87,11 +136,64 @@ class SchedulerRequests(DispatchBot):
         # Send request to change night (don't send nulls in JSON)
         args = locals()
         request = {k: v for k, v in args.items() if v is not None and k != "self"}
-        status, msg = self.send_request(f"del_req {json.dumps(request)}")
+        status, msg = self._send_request(f"del_req {json.dumps(request)}")
 
         if status == "ERROR":
-            self.log({"runtime_error": msg}, level="ERROR")
+            self.logger.error(msg)
             raise RuntimeError(msg)
 
         # Return number of lines removed
         return msg
+
+    def _send_request(self, request):
+        # Send bytes
+        send_msg(sock=self.sock, msg=bytes(request, "ascii"))
+        # Debug log
+        self.logger.debug({'status': 'sending_message', 'request': request})
+
+        # Wait a sec and recieve response
+        time.sleep(1)
+        msg = recv_msg(sock=self.sock).decode('ascii')
+
+        status = msg.split()[0]
+        response = ' '.join(msg.split()[1:])
+
+        # Report and remove
+        message = dict({
+            "request": request,
+            "status": status,
+            "response": response
+        })
+        self.logger.debug({'status': 'recieved_response', 'request': message})
+
+        return status, response
+
+
+########################################################################
+# Utility functions for communicating with Robo88 Scheduler
+########################################################################
+def recvall(sock, n):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+
+def send_msg(sock, msg):
+    # Prefix each message with a 4-byte length (network byte order)
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
